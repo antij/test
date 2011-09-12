75141d740558eca8ec141daec202ccdcd783d88d
antij <antij@cwdi.net>
2011-09-11 18:07:36 -0600

    hotfix changed index

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
+
+
+// hotfix branch
+
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
